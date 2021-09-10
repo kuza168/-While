d88c29bf668cd1cae5a710d7787d973c441e70ce
Kuza168 <anlikes33@gmail.com>
2021-09-10 09:00:06 +0500

    Добавьте файлы проекта.

---
+++ Цикл While.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+float main()
+{
+	setlocale(0, "");
+	float i = 0;
+	float sum = 0;
+	while (i < 5000)
+	{
+		i++;
+		sum += i;
+	}
+	cout << "Сумма чисел от 1 до 5000 = " << sum << endl;
+	cout << " Конечное число = " << i << endl;
+	return 0;
+}
